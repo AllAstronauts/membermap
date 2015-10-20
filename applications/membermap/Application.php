4486a11fbf41619df720d018c179fc5d7c36a15b
Stuart Silvester <ssilvester@invisionpower.com>
2015-10-20 19:08:05 +0100

    Initial skeleton code commit

---
+++ applications/membermap/Application.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @brief		Member Map Application Class
+ * @author		<a href='http://ipb.silvesterwebdesigns.com'>Stuart Silvester & Martin Aronsen</a>
+ * @copyright	(c) 2015 Stuart Silvester & Martin Aronsen
+ * @package		IPS Social Suite
+ * @subpackage	Member Map
+ * @since		20 Oct 2015
+ * @version		
+ */
+ 
+namespace IPS\membermap;
+
+/**
+ * Member Map Application Class
+ */
+class _Application extends \IPS\Application
+{
+
+}
