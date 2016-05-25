2a6c32494d72fbcb134b2a99be0a7bbf9e15c719
Martin Aronsen <m@rtin.no>
2016-05-25 21:10:53 +0200

    Need to check for indexes before trying to add them.

--- setup/upg_110003/upgrade.php
+++ setup/upg_110003/upgrade.php
@@ -23,23 +23,32 @@ class _Upgrade
 	public function step1()
 	{
 		/* These changes should have been done by the 3.1.0 upgrader, but I forgot. */
-		\IPS\Db::i()->addIndex( 'membermap_markers_groups', array(
-			'type'			=> 'key',
-			'name'			=> 'group_position',
-			'columns'		=> array( 'group_position' )
-		) );
+		if ( ! \IPS\Db::i()->checkForIndex( 'membermap_markers_groups', 'group_position' ) )
+		{
+			\IPS\Db::i()->addIndex( 'membermap_markers_groups', array(
+				'type'			=> 'key',
+				'name'			=> 'group_position',
+				'columns'		=> array( 'group_position' )
+			) );
+		}
 
-		\IPS\Db::i()->addIndex( 'membermap_markers', array(
-			'type'			=> 'key',
-			'name'			=> 'marker_member_id',
-			'columns'		=> array( 'marker_member_id' )
-		) );
+		if ( ! \IPS\Db::i()->checkForIndex( 'membermap_markers', 'marker_member_id' ) )
+		{
+			\IPS\Db::i()->addIndex( 'membermap_markers', array(
+				'type'			=> 'key',
+				'name'			=> 'marker_member_id',
+				'columns'		=> array( 'marker_member_id' )
+			) );
+		}
 
-		\IPS\Db::i()->addIndex( 'membermap_markers', array(
-			'type'			=> 'key',
-			'name'			=> 'marker_parent_id',
-			'columns'		=> array( 'marker_parent_id' )
-		) );
+		if ( ! \IPS\Db::i()->checkForIndex( 'membermap_markers', 'marker_parent_id' ) )
+		{
+			\IPS\Db::i()->addIndex( 'membermap_markers', array(
+				'type'			=> 'key',
+				'name'			=> 'marker_parent_id',
+				'columns'		=> array( 'marker_parent_id' )
+			) );
+		}
 
 		\IPS\Db::i()->changeColumn( 'membermap_markers', 'marker_open', array(
 			'name'			=> 'marker_open',
