01a5b929453335e81f9d3c6754e0db199af83127
Martin Aronsen <m@rtin.no>
2016-07-11 22:43:25 +0200

    Make sure $maps is an array

--- setup/upg_working/upgrade.php
+++ setup/upg_working/upgrade.php
@@ -25,16 +25,19 @@ class _Upgrade
 		/* Remove MapQuest.OSM, as it's no longer "free" */
 		$maps = json_decode( \IPS\Settings::i()->membermap_activemaps, true );
 
-		$osm = array_search( 'MapQuestOpen.OSM', $maps['basemaps'] );
-
-		if ( $osm !== FALSE )
+		if ( is_array( $maps ) )
 		{
-			$maps['basemaps'][ $osm ] = 'OpenStreetMap.France';
-			$maps['basemaps'] = array_unique( $maps['basemaps'], SORT_REGULAR );
+			$osm = array_search( 'MapQuestOpen.OSM', $maps['basemaps'] );
 
-			\IPS\Settings::i()->membermap_activemaps = json_encode( $maps );
-			\IPS\Db::i()->update( 'core_sys_conf_settings', array( 'conf_value' => \IPS\Settings::i()->membermap_activemaps ), array( 'conf_key=?', 'membermap_activemaps' ) );
-			unset( \IPS\Data\Store::i()->settings );
+			if ( $osm !== FALSE )
+			{
+				$maps['basemaps'][ $osm ] = 'OpenStreetMap.France';
+				$maps['basemaps'] = array_unique( $maps['basemaps'], SORT_REGULAR );
+
+				\IPS\Settings::i()->membermap_activemaps = json_encode( $maps );
+				\IPS\Db::i()->update( 'core_sys_conf_settings', array( 'conf_value' => \IPS\Settings::i()->membermap_activemaps ), array( 'conf_key=?', 'membermap_activemaps' ) );
+				unset( \IPS\Data\Store::i()->settings );
+			}
 		}
 
 
