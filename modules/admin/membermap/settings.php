811c0cb86b11408d5c74b43a002c803758d0dc0c
Martin Aronsen <m@rtin.no>
2016-12-03 10:19:14 +0100

    Show all profile fields in ACP settings, regardless of access level.

--- modules/admin/membermap/settings.php
+++ modules/admin/membermap/settings.php
@@ -70,10 +70,12 @@ class _settings extends \IPS\Dispatcher\Controller
 			$form->addHeader( 'membermap_autoUpdate' );
 
 			$profileFields = array( '' => ' -- ' . \IPS\Member::loggedIn()->language()->addToStack( 'membermap_profileLocationField' ) . ' -- ' );
-			foreach ( \IPS\core\ProfileFields\Field::fields( array(), \IPS\core\ProfileFields\Field::PROFILE ) as $group => $fields )
+			foreach ( \IPS\core\ProfileFields\Field::fieldData() as $group => $fields )
 			{
 				foreach ( $fields as $id => $field )
 				{
+					$field = \IPS\core\ProfileFields\Field::constructFromData( $field )->buildHelper();
+					
 					$profileFields[ 'core_pfieldgroups_' . $group ][ $id ] = $field->name;
 				}
 			}
