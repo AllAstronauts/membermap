25a77eb5d8defce330d9f6e036341154a356bd51
Martin Aronsen <m@rtin.no>
2017-07-19 10:56:00 +0200

    Build 3.2.1

--- setup/upg_110019/upgrade.php
+++ setup/upg_110019/upgrade.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace IPS\membermap\setup\upg_working;
+namespace IPS\membermap\setup\upg_110019;
 
 /* To prevent PHP errors (extending class does not exist) revealing path */
 if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
@@ -11,7 +11,7 @@ if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
 }
 
 /**
- * {version_human} Upgrade Code
+ * 3.2.1 Upgrade Code
  */
 class _Upgrade
 {
