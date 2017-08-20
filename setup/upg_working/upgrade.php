8eaa3e996bd1df7adfa53432081fe5956ecc8aff
Martin Aronsen <m@rtin.no>
2017-08-20 17:38:46 +0200

    Set the comment and review perm to the same as "add marker".

--- setup/upg_working/upgrade.php
+++ setup/upg_working/upgrade.php
@@ -19,6 +19,8 @@ class _Upgrade
 	{
 		\IPS\Task::queue( 'core', 'RebuildContainerCounts', array( 'class' => 'IPS\membermap\Markers\Groups', 'count' => 0 ), 5, array( 'class' ) );
 
+		\IPS\Db::i()->update( 'core_permission_index', 'perm_4=perm_3, perm_5=perm_3', array( 'app=?', 'membermap' ) );
+
 		return TRUE;
 	}
 }
