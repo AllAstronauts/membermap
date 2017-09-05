50033f9a36d6525f264293068d2cbd39175a7401
Martin Aronsen <m@rtin.no>
2017-09-05 20:36:34 +0200

    Need to get this out quick to fix a fatal bug with club deletion.

--- hooks/memberClub.php
+++ hooks/memberClub.php
@@ -18,16 +18,19 @@ class membermap_hook_memberClub extends _HOOK_CLASS_
 	{
 		$return = call_user_func_array( 'parent::nodes', func_get_args() );
 
-		if ( \IPS\Settings::i()->membermap_clubs_showInClubHeader )
+		if ( \IPS\Dispatcher::hasInstance() AND \IPS\Dispatcher::i()->controllerLocation == 'front' )
 		{
-			if( \IPS\Settings::i()->membermap_clubs == '*' OR \strstr( ','.\IPS\Settings::i()->membermap_clubs.',', ",{$this->id}," ) )
+			if ( \IPS\Settings::i()->membermap_clubs_showInClubHeader )
 			{
-				$return[] = array(
-					'name'			=> "Member Map",
-					'url'			=> \IPS\Http\Url::internal( 'app=membermap&module=membermap&controller=showmap&filter=showClub&clubId=' . $this->id, 'front', 'membermap' ),
-					'node_class'	=> 'IPS\membermap\Markers\Groups',
-					'node_id'		=> \IPS\membermap\Map::i()->getMemberGroupId(),
-				);
+				if( \IPS\Settings::i()->membermap_clubs == '*' OR \strstr( ','.\IPS\Settings::i()->membermap_clubs.',', ",{$this->id}," ) )
+				{
+					$return[] = array(
+						'name'			=> \IPS\Member::loggedIn()->language()->addToStack('membermap_clubnavtitle'),
+						'url'			=> \IPS\Http\Url::internal( 'app=membermap&module=membermap&controller=showmap&filter=showClub&clubId=' . $this->id, 'front', 'membermap' ),
+						'node_class'	=> 'IPS\membermap\Markers\Groups',
+						'node_id'		=> \IPS\membermap\Map::i()->getMemberGroupId(),
+					);
+				}
 			}
 		}
 
