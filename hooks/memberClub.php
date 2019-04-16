9917247ba3fae1ed26fa9b8ace0cc85529ee08cb
Martin Aronsen <m@rtin.no>
2019-04-16 18:08:28 +0200

    Fixing issue #50. Cron task bug due to dispatcher not being set up

--- hooks/memberClub.php
+++ hooks/memberClub.php
@@ -46,7 +46,7 @@ class membermap_hook_memberClub extends _HOOK_CLASS_
 	{
 		parent::save();
 
-		if ( \IPS\Settings::i()->membermap_clubsExt AND \IPS\Settings::i()->clubs_locations AND \IPS\Dispatcher::i()->controllerLocation == 'admin' )
+		if ( \IPS\Settings::i()->membermap_clubsExt AND \IPS\Settings::i()->clubs_locations AND \IPS\Disptacher::hasInstance() AND \IPS\Dispatcher::i()->controllerLocation == 'admin' )
 		{
 			\IPS\membermap\Map::i()->invalidateJsonCache();
 		}
